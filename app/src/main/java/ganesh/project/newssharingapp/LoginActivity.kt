575713c239d9089296090152c53d763ddd9cd483
bethiganesh94 <bethiganesh94@gmail.com>
2025-11-05 23:25:59 +0530

    Implemented Login screen in the app

---
+++ app/src/main/java/ganesh/project/newssharingapp/LoginActivity.kt
@@ -0,0 +1,209 @@
+package ganesh.project.newssharingapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreen()
+        }
+    }
+}
+
+
+@Composable
+fun LoginScreen()
+{
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current as Activity
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.bg_main)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Welcome",
+                color = Color.White,
+                style = TextStyle(fontSize = 52.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Login to your create Account",
+                color = Color.White,
+                style = MaterialTheme.typography.headlineSmall.copy(fontSize = 16.sp)
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = colorResource(R.color.tran_white),
+                focusedContainerColor = colorResource(R.color.tran_white),
+                unfocusedTextColor = Color.White,
+            ),
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Enter Email") },
+            textStyle = TextStyle(color = colorResource(id = R.color.white)),
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.email),
+                    contentDescription = ""
+
+                )
+            },
+        )
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = colorResource(R.color.tran_white),
+                focusedContainerColor = colorResource(R.color.tran_white),
+                unfocusedTextColor = Color.White,
+            ),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter Password") },
+            textStyle = TextStyle(color = colorResource(id = R.color.white)),
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.password),
+                    contentDescription = ""
+
+                )
+            },
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            modifier = Modifier
+                .clickable {
+                    when {
+                        email.isEmpty() -> {
+                            Toast.makeText(context, " Please Enter Mail", Toast.LENGTH_SHORT).show()
+                        }
+
+                        password.isEmpty() -> {
+                            Toast.makeText(context, " Please Enter Password", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+
+                        else -> {
+
+                        }
+
+                    }
+                }
+                .width(350.dp)
+                .background(
+                    color = colorResource(id = R.color.bt_color),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .border(
+                    width = 1.dp,
+                    color = colorResource(id = R.color.bt_color),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .padding(vertical = 12.dp)
+                .align(Alignment.CenterHorizontally),
+            text = "Login",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge.copy(
+                color = Color.White,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+
+            Text(
+                modifier = Modifier,
+                text = "New Here?  ",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium.copy(
+                    color = colorResource(id = R.color.white),
+                )
+            )
+
+
+            Text(
+                modifier = Modifier
+                    .clickable {
+                        context.startActivity(Intent(context, RegistrationActivity::class.java))
+                        context.finish()
+                    },
+                text = "Create Account",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium.copy(
+                    color = colorResource(id = R.color.white),
+                )
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(46.dp))
+    }
+}
--- app/src/main/java/ganesh/project/newssharingapp/MainActivity.kt
+++ app/src/main/java/ganesh/project/newssharingapp/MainActivity.kt
@@ -1,47 +1,108 @@
 package ganesh.project.newssharingapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import ganesh.project.newssharingapp.ui.theme.NewsSharingAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NewsSharingAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            EntryScreenMA()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun EntryScreenMA() {
+    val context = LocalContext.current as Activity
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+
+        context.startActivity(Intent(context, LoginActivity::class.java))
+
+
+    }
+
+    EntryScreen()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NewsSharingAppTheme {
-        Greeting("Android")
+fun EntryScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+            Spacer(modifier = Modifier.weight(1f))
+
+
+            Image(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                painter = painterResource(id = R.drawable.icon_news_sharing),
+                contentDescription = "News Sharing",
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = "Welcome To",
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = "News Sharing App\nby Ganesh",
+                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+
+        }
     }
+
 }
